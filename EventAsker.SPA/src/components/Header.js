e772d1514a3bc6b588d17cbe8306d761ac67774a
Dominik <dominik.slapa@gmail.com>
2018-03-31 23:34:12 +0200

    Register form added, navbar added, React-Router installed

---
+++ EventAsker.SPA/src/components/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import '../index.css';
+
+
+export default class Header extends Component {
+    render(){
+        return(
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                            <Link className="navbar-brand" to="/">EventAsker</Link>
+                    </div>  
+                        <ul className="nav navbar-nav flex-row">
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/login">Login</Link></li>
+                            <li><Link to="/register">Register</Link></li>                         
+                        </ul>
+                 </div>
+                
+            </nav>
+        );
+    }
+}
