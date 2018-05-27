1e502288a69629248c2391e62a03747d5ae7e31c
EnjoyTheNoise <jankos.pdp@gmail.com>
2018-05-28 00:50:51 +0200

    fix constants

--- EventAsker.SPA/src/constants.js
+++ EventAsker.SPA/src/constants.js
@@ -1,4 +1,3 @@
 export const BASE_URL = "http://localhost:59611/api"
 export const BASE_STATIC_FILES = "http://localhost:59611/upload/"
-//export const BASE_URL = "http://localhost:59616/api"
-export const BASE_URL = "http://localhost:3372/api" //Janek
+//export const BASE_URL = "http://localhost:3372/api" //Janek
